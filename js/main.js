c244b8b258b470a5c0fd7631778900560bc63b46
ahmedmersal <37100454+ahmedmersal@users.noreply.github.com>
2019-03-31 02:09:33 +0200

    update 2
    add jquery

--- js/main.js
+++ js/main.js
@@ -1,26 +1,72 @@
-
 $(document).ready(function () {
     'use strict';
+    $(this).scrollTop(0);
+
     
-    $(".toggle").click(function () {
+    //header-content
+    $(".content").animate({top: '25%'}, 1000);
+    
+    
+    
+    //ABOUT
+    
+    $(window).on("scroll", function () {
+ 
+        var sc = $(window).scrollTop();
         
-        $("ul").slideToggle(300);
+        
+        if (sc > 238) {
+            
+            $("#about").fadeTo(1000, 1);
+            
+        }
     });
+
+    
+    //skills
+    $(window).on("scroll", function () {
+ 
+        var sc = $(window).scrollTop();
+        
+        
+        if (sc > 710) {
+            
+            $("#html").fadeTo(800, 1);
+            $("#css").fadeTo(900, 1);
+            $("#Bootstrap").fadeTo(1100, 1);
+            $("#jquery").fadeTo(1200, 1);
+            $("#sass").fadeTo(1300, 1);
+            $("#gulp").fadeTo(1400, 1);
+            
+        }
+    });
+
+        
+    
+    //skills
+    $(window).on("scroll", function () {
+ 
+        var sc = $(window).scrollTop();
+                
+        if (sc > 1416) {
+            
+            $("#p-row,#p-row4").animate({right: "0"}, 800);
+            $("#p-row2").animate({top: "0"}, 800);
+            $("#p-row3,#p-row6").animate({left: "0"}, 800);
+            $("#p-row5").animate({bottom: "0"}, 1000);
+           
+            
+        }
+    });
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
 });
 
-$('.carousel[data-type="multi"] .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-    	next = $(this).siblings(':first');
-  	}
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
