ebe8dd22a6618c3bec3637667fb2b7ad5aa8ad5d
ahmedmersal <ahmedmersal>
2019-09-25 07:17:51 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,72 +1,46 @@
-$(document).ready(function () {
-    'use strict';
-    $(this).scrollTop(0);
+$(document).ready(function() {
+  'use strict';
+  $(this).scrollTop(0);
 
-    
-    //header-content
-    $(".content").animate({top: '25%'}, 1000);
-    
-    
-    
-    //ABOUT
-    
-    $(window).on("scroll", function () {
- 
-        var sc = $(window).scrollTop();
-        
-        
-        if (sc > 238) {
-            
-            $("#about").fadeTo(1000, 1);
-            
-        }
-    });
+  //header-content
+  $('.content').animate({ top: '25%' }, 1000);
 
-    
-    //skills
-    $(window).on("scroll", function () {
- 
-        var sc = $(window).scrollTop();
-        
-        
-        if (sc > 710) {
-            
-            $("#html").fadeTo(800, 1);
-            $("#css").fadeTo(900, 1);
-            $("#Bootstrap").fadeTo(1100, 1);
-            $("#jquery").fadeTo(1200, 1);
-            $("#sass").fadeTo(1300, 1);
-            $("#gulp").fadeTo(1400, 1);
-            
-        }
-    });
+  //ABOUT
 
-        
-    
-    //skills
-    $(window).on("scroll", function () {
- 
-        var sc = $(window).scrollTop();
-                
-        if (sc > 1416) {
-            
-            $("#p-row,#p-row4").animate({right: "0"}, 800);
-            $("#p-row2").animate({top: "0"}, 800);
-            $("#p-row3,#p-row6").animate({left: "0"}, 800);
-            $("#p-row5").animate({bottom: "0"}, 1000);
-           
-            
-        }
-    });
+  $(window).on('scroll', function() {
+    var sc = $(window).scrollTop();
 
-        
-    
-    
-    
-    
-    
-    
-    
-    
+    if (sc > 238) {
+      $('#about').fadeTo(1000, 1);
+    }
+  });
+
+  //skills
+  $(window).on('scroll', function() {
+    var sc = $(window).scrollTop();
+
+    if (sc > 710) {
+      $('#html').fadeTo(800, 1);
+      $('#css').fadeTo(900, 1);
+      $('#Bootstrap').fadeTo(1100, 1);
+      $('#react').fadeTo(1200, 1);
+      $('#sass').fadeTo(1300, 1);
+      $('#gulp').fadeTo(1400, 1);
+      $('#js').fadeTo(1500, 1);
+      $('#jQuery').fadeTo(1600, 1);
+      $('#Git').fadeTo(1700, 1);
+    }
+  });
+
+  //skills
+  $(window).on('scroll', function() {
+    var sc = $(window).scrollTop();
+
+    if (sc > 1416) {
+      $('#p-row,#p-row4').animate({ right: '0' }, 800);
+      $('#p-row2').animate({ top: '0' }, 800);
+      $('#p-row3,#p-row6').animate({ left: '0' }, 800);
+      $('#p-row5').animate({ bottom: '0' }, 1000);
+    }
+  });
 });
-
